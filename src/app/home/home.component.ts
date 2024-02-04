a4372f96a531f0411e3018f6e744d73e5eff1171
Navindu <dahanayakenw.20@uom.lk>
2024-02-04 23:20:47 +0530

    add food images to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public foods: string[] =[];
+
+  constructor(private foodService: FoodService) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAll();
+  }
+
 
 }
