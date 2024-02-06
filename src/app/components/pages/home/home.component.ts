0b19df29c3912b9f80c323dae2362272f39981ab
Navindu <dahanayakenw.20@uom.lk>
2024-02-07 02:32:35 +0530

    Search component finished

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../../../services/food/food.service';
 import { Food } from '../../../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,17 @@ import { Food } from '../../../shared/models/Food';
 export class HomeComponent implements OnInit{
   public foods: Food[] =[];
 
-  constructor(private foodService: FoodService) { 
-    this.foods = this.foodService.getAll();
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+
+    activatedRoute.params.subscribe((params)=> {
+      if(params.searchTerm){
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    }) 
   }
+
   ngOnInit(): void {
     
   }
