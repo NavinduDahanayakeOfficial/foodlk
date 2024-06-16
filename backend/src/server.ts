3697ee14972d343eed0db368d4334cbccf1f0383
Navindu <dahanayakenw.20@uom.lk>
2024-06-17 00:53:35 +0530

    added @types/express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-app.use(express.static("public"));
+app.use(express.static("public/browser"));
 app.get("*", (req, res) => {
    res.sendFile(path.join(__dirname, "public", "browser" , "index.html"));
 })
