af17294e9901a9a659047bcf5fd231ea59dd0a0c
Navindu <dahanayakenw.20@uom.lk>
2024-06-17 00:38:02 +0530

    changes made to deploy the app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
@@ -10,7 +11,7 @@ import { dbConnect } from "./configs/database.config";
 const app = express();
 app.use(express.json());
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 dbConnect();
 
 app.use(
@@ -24,7 +25,12 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port = 5000;
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "public", "browser" , "index.html"));
+})
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
